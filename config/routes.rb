0eac2b576bd09de9d4dc5b759ad9bccae0de9999
joseramirez270 <jose.ramirez270@gmail.com>
2017-03-15 23:27:04 -0400

    Made small adjustments to form box sizes

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,4 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
