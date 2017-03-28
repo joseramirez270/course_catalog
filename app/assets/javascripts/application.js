f3926a110d3c4a227535c6344f3e880d3b997797
joseramirez270 <jose.ramirez270@gmail.com>
2017-03-28 14:20:46 -0400

    Remove require-turbolinks from application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,12 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
 
 // makes search dynamic
 $(function() {
-  $("#course_search input").keyup('turbolinks:load', function(){
+  $("#course_search input").keyup(function(){
     $.get($("#course_search").attr("action"), $("#course_search").serialize(), null, "script");
     return false;
   });
